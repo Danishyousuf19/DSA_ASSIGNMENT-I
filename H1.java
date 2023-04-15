37b9766599f21a3db7ead8aa7c8b9129c7152c25
Danishyousuf19 <130566129+Danishyousuf19@users.noreply.github.com>
2023-04-15 17:24:40 +0530

    Update H1.java

--- H1.java
+++ H1.java
@@ -1,24 +1,19 @@
-
+/*Write a Java program that takes as input three integers, a, b, and c, from the Java console
+and determines if they can be used in a correct arithmetic formula (in the given order), like
+“a + b = c,” “a = b − c,” or “a b = c.” ∗/
 public class H1 {
 	public static void main(String[] args) {
-	   int a[][]= {{4, 2, 7},{4, 2, 7}};
-	   int b[][]= {{4, 2, 7},{4, 2, 7}};
-	 
-	    {
-	      int x= a.length;
-	      int y= a[0].length;
-	        int c[][]=new int [x][y];
-	        for(int i=0;i<  a.length;i++){
-	            for(int j=0;j<a[i].length;j++){
-	                c[i][j]= a[i][j]+ b[i][j];
-	            }
-	        }for(int i=0;i<c.length;i++){
-	            for(int j=0;j< c[i].length;j++){
-	                System.out.print(c[i][j]+" ");
-	                
-	            } System.out.println();
-	        }
-	        
-	    }
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 3 no.s");
+int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+if(a+b==c) {
+	System.out.println("a+b=c can be used");
+}
+else if(b-c==a) {
+	System.out.println("b-c=a can be used");
+}
+else if(a*b==c) {
+	System.out.println("a*b=c can be used");
+}
 	}
 }
