8009362a52c111ef5a0dee328c27fb8847693e2a
Danishyousuf19 <130566129+Danishyousuf19@users.noreply.github.com>
2023-04-15 17:33:20 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,10 +1,22 @@
 public class Q4 {
-    
+    //generate all possible combinations of chracters +++++ car
     public static void main(String[] args) {
         String input = "car";
-        generateStrings("", input);
+       // generateStrings("", input);
+        easway(inputed);
     }
-    
+     public  static void easway(String d) {
+    	
+    	for(int i=0;i<3;i++) {
+    		for(int j=0;j<3;j++) {
+    			for(int k=0;k<3;k++) {
+    				if(i!=j&&i!=k&&k!=j) {
+    					System.out.println(d.charAt(i)+""+d.charAt(j)+""+d.charAt(k));
+    				}	
+    			}
+    		}
+    		
+    	}
     private static void generateStrings(String prefix, String remaining) {
         int n = remaining.length();System.err.println(n);System.err.println(remaining);
         if (n == 0) {
